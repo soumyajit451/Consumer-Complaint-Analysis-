88aee68815db89d304b3e2a2b1427aa399215394
soumyajit <soumyajitsahu2013rta@gmail.com>
2024-06-26 18:21:03 +0530

    data pipeline running

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import logging
 from yaml import safe_load
 import pandas as pd
@@ -7,20 +8,18 @@ from sklearn.model_selection import train_test_split
 from src.logger import create_log_path, CustomLogger
 
 log_file_path = create_log_path('make_dataset')
-# create the custom logger object
+# Create the custom logger object
 dataset_logger = CustomLogger(logger_name='make_dataset',
                               log_filename=log_file_path)
-# set the level of logging to INFO
+# Set the level of logging to INFO
 dataset_logger.set_log_level(level=logging.INFO)
 
-
-def load_raw_data(input_path: Path) ->  pd.DataFrame:
+def load_raw_data(input_path: Path) -> pd.DataFrame:
     raw_data = pd.read_csv(input_path)
     rows, columns = raw_data.shape
     dataset_logger.save_logs(msg=f'{input_path.stem} data read having {rows} rows and {columns} columns',
                              log_level='info')
     return raw_data
-
 
 def train_val_test_split(data: pd.DataFrame,
                          val_size: float,
@@ -38,20 +37,15 @@ def train_val_test_split(data: pd.DataFrame,
                              log_level='info')
     return train_data, val_data, test_data
 
-
-
-
-def save_data(data: pd.DataFrame,output_path: Path):
-    data.to_csv(output_path,index=False)
+def save_data(data: pd.DataFrame, output_path: Path):
+    data.to_csv(output_path, index=False)
     dataset_logger.save_logs(msg=f'{output_path.stem + output_path.suffix} data saved successfully to the output folder',
                              log_level='info')
-    
 
 def read_params(input_file):
     try:
         with open(input_file) as f:
             params_file = safe_load(f)
-
     except FileNotFoundError as e:
         dataset_logger.save_logs(msg='Parameters file not found, switching to default values for train test split',
                                  log_level='error')
@@ -60,7 +54,6 @@ def read_params(input_file):
         test_size = default_dict['test_size']
         random_state = default_dict['random_state']
         return val_size, test_size, random_state
-    
     else:
         dataset_logger.save_logs(msg='Parameters file read successfully',
                                  log_level='info')
@@ -70,10 +63,11 @@ def read_params(input_file):
         return val_size, test_size, random_state
 
 def complaint():
+    time.sleep(10)
     # Read the input file name from command line
     input_file_name = sys.argv[1]
     # Current file path
-    current_path = Path(__file__)
+    current_path = Path(__file__).resolve()
     # Root directory path
     root_path = current_path.parent.parent.parent
     # Interim data directory path
@@ -81,7 +75,7 @@ def complaint():
     # Make directory for the interim path
     interim_data_path.mkdir(exist_ok=True)
     # Raw train file path
-    raw_df_path = root_path / 'data' / 'extracted_data' /  input_file_name
+    raw_df_path = root_path / 'data' / 'extracted_data' / input_file_name
     # Load the training file
     raw_df = load_raw_data(input_path=raw_df_path)
     # Parameters from params file
@@ -95,14 +89,12 @@ def complaint():
     target_column = 'Consumer disputed?'
     if target_column in test_df.columns:
         test_df = test_df.drop(columns=[target_column])
-    
     # Save the train data to the output path
     save_data(data=train_df, output_path=interim_data_path / 'train.csv')
     # Save the validation data to the output path
     save_data(data=val_df, output_path=interim_data_path / 'val.csv')
     # Save the test data to the output path
     save_data(data=test_df, output_path=interim_data_path / 'test.csv')
-    
-    
+
 if __name__ == '__main__':
     complaint()
